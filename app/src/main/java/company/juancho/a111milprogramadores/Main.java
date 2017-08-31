30bf974a8664009c0a9aa9d9989f5b277240f2d7
ayelen7 <machunita@gmail.com>
2017-08-30 21:37:42 -0300

    30-08

--- app/src/main/java/company/juancho/a111milprogramadores/Main.java
+++ app/src/main/java/company/juancho/a111milprogramadores/Main.java
@@ -69,6 +69,9 @@ public class Main extends AppCompatActivity
         } else if (id == R.id.nav_eventos) {
             this.lanzarWeb();
 
+        } else if (id == R.id.nav_map) {
+            this.lanzarMapa();
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -95,7 +98,7 @@ public class Main extends AppCompatActivity
 
 
 
-    public void lanzarMapa(View v){
+    public void lanzarMapa(){
         double lon = -60.669510, lat = -32.929058;
 
 
@@ -103,14 +106,7 @@ public class Main extends AppCompatActivity
         Uri uri = Uri.parse("geo:"+lat+","+"lon"+"?z=17&q=" +
                 lat+","+lon+"(Rosario)");
         startActivity( new Intent(Intent.ACTION_VIEW, uri));
-        /*
-        Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?saddr="+_lat+","+_ltg+"&daddr="+destino_lat+","+destino_ltg)); //o la direccion/consulta que quiera "http://maps.google.com/maps?q="+ myLatitude  +"," + myLongitude +"("+ labLocation + ")&iwloc=A&hl=es"
-        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
-        startActivity(intent);
 
-
-        https://www.google.com.ar/search?q=metropolitano+rosario&npsic=0&rflfq=1&rlha=0&rllag=-32928944,-60670131,72&tbm=lcl&ved=0ahUKEwi26fjG-_LVAhWJIJAKHTyKAoIQtgMIJg&tbs=lrf:!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2&rldoc=1#rlfi=hd:;si:8224927032178275017;mv:!1m3!1d9557.227714970175!2d-60.65440825!3d-32.937335049999994!2m3!1f0!2f0!3f0!3m2!1i427!2i274!4f13.1
-        */
     }
 
 
